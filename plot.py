4c1041db402dcf0103e3f781298841dd755b124f
Cameron <cameron.farzaneh@gmail.com>
2018-03-08 10:18:51 -0500

    Working on quick_sort

--- plot.py
+++ plot.py
@@ -1,20 +1,22 @@
 import matplotlib.pyplot as plt
+from scipy.interpolate import spline
 import numpy as np
 from numpy import genfromtxt
 
 print("Plotting in Python using matplotlib...")
 
 my_data = genfromtxt('output.txt', delimiter=',')
-print(my_data.shape);
+print(my_data);
 
-x_orig = [10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000]
-x = np.arange(1000)
-y = x**2
+x_orig = np.array([10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000])
 
-m, b = np.polyfit(x, y, 1)
+plt.scatter(x_orig, my_data, color='r')
 
-plt.scatter(x_orig,my_data, color='r')
-plt.plot(x, x**2, '-')
+xnew = np.linspace(x_orig.min(),x_orig.max(),100)
+power_smooth = spline(x_orig,my_data,xnew)
+
+plt.plot(xnew, power_smooth)
+
 plt.title('Insertion Sort')
 
 plt.show()
--- quick_sort.cpp
+++ quick_sort.cpp
@@ -2,8 +2,11 @@
 #include <cstdlib>
 #include <chrono>
 
-int* insertion_sort(int *arr, size_t len);
+void quickSort(int *arr, int low, int high);
+int partition(int arr[], int low, int high);
 int* initialize_array(int *arr, size_t size);
+void swap(int* a, int* b);
+void printArray(int arr[], int size);
 
 int main() {
 	
@@ -36,68 +39,79 @@ int main() {
 
 	fp = fopen("output.txt","w");
 	
+	int n = sizeof(arr1)/sizeof(arr1[0]);
 	auto start = std::chrono::high_resolution_clock::now();
-	insertion_sort(arr1,10);
+	quickSort(arr1, 0, n-1);
 	auto end = std::chrono::high_resolution_clock::now();
 	double elapsed_time = double(std::chrono::duration_cast <std::chrono::nanoseconds> (end-start).count());
 	fprintf(fp,"%f,",elapsed_time/1e9);
 
+	n = sizeof(arr2)/sizeof(arr2[0]);
 	start = std::chrono::high_resolution_clock::now();
-	insertion_sort(arr2,50);
+	quickSort(arr2, 0, n-1);
 	end = std::chrono::high_resolution_clock::now();
 	elapsed_time = double(std::chrono::duration_cast <std::chrono::nanoseconds> (end-start).count());
 	fprintf(fp,"%f,",elapsed_time/1e9);
 
+	n = sizeof(arr3)/sizeof(arr3[0]);
 	start = std::chrono::high_resolution_clock::now();
-	insertion_sort(arr3,100);
+	quickSort(arr3, 0, n-1);
 	end = std::chrono::high_resolution_clock::now();
 	elapsed_time = double(std::chrono::duration_cast <std::chrono::nanoseconds> (end-start).count());
 	fprintf(fp,"%f,",elapsed_time/1e9);
 
+	n = sizeof(arr4)/sizeof(arr4[0]);
 	start = std::chrono::high_resolution_clock::now();
-	insertion_sort(arr4,500);
+	quickSort(arr4, 0, n-1);
 	end = std::chrono::high_resolution_clock::now();
 	elapsed_time = double(std::chrono::duration_cast <std::chrono::nanoseconds> (end-start).count());
 	fprintf(fp,"%f,",elapsed_time/1e9);
 
+	n = sizeof(arr5)/sizeof(arr5[0]);
 	start = std::chrono::high_resolution_clock::now();
-	insertion_sort(arr5,1000);
+	quickSort(arr5, 0, n-1);
 	end = std::chrono::high_resolution_clock::now();
 	elapsed_time = double(std::chrono::duration_cast <std::chrono::nanoseconds> (end-start).count());
 	fprintf(fp,"%f,",elapsed_time/1e9);
 
+	n = sizeof(arr6)/sizeof(arr6[0]);
 	start = std::chrono::high_resolution_clock::now();
-	insertion_sort(arr6,5000);
+	quickSort(arr6, 0, n-1);
 	end = std::chrono::high_resolution_clock::now();
 	elapsed_time = double(std::chrono::duration_cast <std::chrono::nanoseconds> (end-start).count());
 	fprintf(fp,"%f,",elapsed_time/1e9);
 
+	n = sizeof(arr7)/sizeof(arr7[0]);
 	start = std::chrono::high_resolution_clock::now();
-	insertion_sort(arr7,10000);
+	quickSort(arr7, 0, n-1);
 	end = std::chrono::high_resolution_clock::now();
 	elapsed_time = double(std::chrono::duration_cast <std::chrono::nanoseconds> (end-start).count());
 	fprintf(fp,"%f,",elapsed_time/1e9);
 
+	n = sizeof(arr8)/sizeof(arr8[0]);
 	start = std::chrono::high_resolution_clock::now();
-	insertion_sort(arr8,50000);
+	quickSort(arr8, 0, n-1);
 	end = std::chrono::high_resolution_clock::now();
 	elapsed_time = double(std::chrono::duration_cast <std::chrono::nanoseconds> (end-start).count());
 	fprintf(fp,"%f,",elapsed_time/1e9);
 
+	n = sizeof(arr9)/sizeof(arr9[0]);
 	start = std::chrono::high_resolution_clock::now();
-	insertion_sort(arr9,100000);
+	quickSort(arr9, 0, n-1);
 	end = std::chrono::high_resolution_clock::now();
 	elapsed_time = double(std::chrono::duration_cast <std::chrono::nanoseconds> (end-start).count());
 	fprintf(fp,"%f,",elapsed_time/1e9);
 
+	n = sizeof(arr10)/sizeof(arr10[0]);
 	start = std::chrono::high_resolution_clock::now();
-	insertion_sort(arr10,500000);
+	quickSort(arr10, 0, n-1);
 	end = std::chrono::high_resolution_clock::now();
 	elapsed_time = double(std::chrono::duration_cast <std::chrono::nanoseconds> (end-start).count());
 	fprintf(fp,"%f,",elapsed_time/1e9);
 
+	n = sizeof(arr11)/sizeof(arr11[0]);
 	start = std::chrono::high_resolution_clock::now();
-	insertion_sort(arr11,1000000);
+	quickSort(arr11, 0, n-1);
 	end = std::chrono::high_resolution_clock::now();
 	elapsed_time = double(std::chrono::duration_cast <std::chrono::nanoseconds> (end-start).count());
 	fprintf(fp,"%f",elapsed_time/1e9);
@@ -114,16 +128,38 @@ int* initialize_array(int *arr, size_t size) {
 	return arr;
 }
 
-int* insertion_sort(int *arr, size_t len) {
-	int i, key, j;
-	for (int i = 1; i < len; ++i) {
-		key = arr[i];
-		j = i-1;
-		while(j >= 0 && arr[j] > key) {
-			arr[j+1] = arr[j];
-			j-=1;
-		}
-		arr[j+1] = key;
+void quickSort(int *arr, int low, int high) {
+	if (low < high) {
+		int pi = partition(arr, low, high);
+
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
 	}
-	return arr;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+ 
+    for (int j = low; j <= high-1; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+void printArray(int arr[], int size) {
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
 }
