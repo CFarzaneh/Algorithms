53657835225a1bbc08e58d57f89d2555729ea661
Cameron <cameron.farzaneh@gmail.com>
2018-04-12 17:43:36 -0400

    Got plotting working!!

--- Prog2/plot.py
+++ Prog2/plot.py
@@ -6,45 +6,50 @@ import pandas as pd
 
 probes = ["linear","quadratic","double","cam"]
 
-# for i in probes:
-# 	nameOfFile = i + "_probe_output.txt"
-# 	print(nameOfFile)
+for i in probes:
+	nameOfFile = i + "_probe_output.txt"
+	print(nameOfFile)
 
-# 	df = pd.read_csv(nameOfFile)
-# 	df.plot(x='x',y='y')
+	df = pd.read_csv(nameOfFile)
+	dfPy = df.values
 
-# 	plt.title(i + ' Probe')
-# 	plt.ylabel('Number of Probes')
-# 	plt.xlabel('Load Factor')
+	x = np.arange(0.5, 1.01, 0.01)
 
-# 	plt.show()
+	arr = []
+	for f in range(0,x.shape[0]-1):
+		sum = 0
+		j = 0
+		low = x[f]
+		high = x[f+1]
+		for k in range(0,dfPy.shape[0]):
+			num = dfPy[k, 0]
+			if num < high and num >= low:
+				sum += dfPy[k, 1]
+				j+=1
+		avg = sum/j
+		arr.append(avg)
 
-i = "double"
-nameOfFile = i + "_probe_output.txt"
-print(nameOfFile)
+	x=np.delete(x,-1)
+	arr = np.asarray(arr)
+	final = np.column_stack((x, arr))
 
-df = pd.read_csv(nameOfFile)
-dfPy = df.values
-dfPy = dfPy[-1]
-cuttoff = dfPy[1]
-df = df[:-1]
-print(df)
-print(cuttoff)
+	df = pd.DataFrame(data=final,columns=['a','Mine'])
+	df.plot(x='a',y='Mine')
 
-df.plot(kind='scatter',x='x',y='y')
+	axes = plt.gca()
+	axes.set_ylim([0,100])
+	axes.set_xlim([0.5,1])
 
-axes = plt.gca()
-axes.set_ylim([0,100])
+	plt.title(i + ' Probe')
+	plt.ylabel('Number of Probes')
+	plt.xlabel('Load Factor')
 
-plt.title(i + ' Probe')
-plt.ylabel('Number of Probes')
-plt.xlabel('Load Factor (Cuttoff at ' + str(cuttoff) + ')')
+	def graph(formula):  
+	    x = np.arange(0.5, 0.999, 0.01)
+	    y = eval(formula)
+	    plt.plot(x, y, color='green', label='1/(1-x) Function')  
 
-def graph(formula):  
-    x = np.arange(0.5, 0.999, 0.01)
-    y = eval(formula)
-    plt.plot(x, y, color='green')  
+	graph('1/(1-x)')
 
-graph('1/(1-x)')
-
-plt.show()
+	plt.legend(loc='best')
+	plt.show()
